225955213b97e8ef7aa5843cb049174690d0311f
Laurent Courty <lrntct@gmail.com>
2025-09-03 19:32:30 -0600

    add function for intersected areas

--- prec_mean.py
+++ prec_mean.py
@@ -5,43 +5,61 @@ import numpy as np
 import geopandas as gpd
 import xarray as xr
 import xvec  # noqa: F401
+import pyproj
 
-from xarray_hydro.watershed_utils import get_grid_from_dataset
+from xarray_hydro.watershed_utils import get_mean_values
 
 
 def main():
     t0 = time()
 
     # project path
-    path = Path(__file__).resolve().parent.parent.parent
-    print(path)
+    base_path = Path(__file__).resolve().parent
+    input_path = base_path / Path("input")
+    output_path = base_path / Path("output")
 
     ## Importar el shape de cuencas
-    watershed_path = path / Path("input") / Path("watershed_area.shp")
+    watershed_path = input_path / Path("watershed_area.shp")
     watershed = gpd.read_file(watershed_path)
     watershed.crs = "epsg:4326"
     watershed = watershed[["nodeID", "area", "geometry"]]
 
+    ## Importar datos ERA5
+    era5_pattern = "era5_total_precipitation_*_hourly_118W-86W_14N-34N_ensemble.nc"
+    era5_files = input_path.glob(era5_pattern)
+
+    ds_era5 = xr.open_mfdataset(
+        era5_files,
+        concat_dim="time",
+        combine="nested",
+        join="outer",  # 2024 tiene solamente 39 celdas de latitude (14.0 a 33.0), y no 41 como las demás
+        compat="no_conflicts",
+        parallel=True,
+    )
+    ds_era5.attrs["crs_wkt"] = pyproj.CRS.from_epsg(4326).to_wkt()
+
+    mean_values = get_mean_values(
+        ds_era5, watershed, catchment_id="nodeID", x_coords="longitude", y_coords="latitude"
+    )
+    print(mean_values)
+
     ############################
     ## Generar shape de poligonos de los pixeles que abarcan los datos de ERA5 descargados
     # Pendiente buscar otra alternativa para generar el grid a partir del raster
-    df_era5 = xr.open_dataset(
-        path
-        / Path("input")
-        / Path("era5_total_precipitation_2020_hourly_118W-86W_14N-34N_ensemble.nc")
-    )
-    grid = get_grid_from_dataset(df_era5)
-    grid.crs = "epsg:4326"
-    print(grid)
+    # df_era5 = xr.open_dataset(
+    #     path
+    #     / Path("input")
+    #     / Path("era5_total_precipitation_2020_hourly_118W-86W_14N-34N_ensemble.nc")
+    # )
+    # grid = get_grid_from_dataset(df_era5)
+    # print(grid)
 
     assert False
 
     ############################
     ## Intersectar las cuencas y el grid.
     intersect = grid.overlay(watershed, how="intersection")
-    intersect.to_file(
-        path / Path("output") / Path("intersect.shp")
-    )  # opcional guardarlo
+    # intersect.to_file(output_path / Path("intersect.shp"))  # opcional guardarlo
 
     ## Generar puntos representativos en los poligonos resultantes de la intersección entre cuencas y el grid.
     intersect_points = intersect.representative_point()
@@ -52,9 +70,7 @@ def main():
     points_id = intersect.sjoin(intersect_points, how="right", predicate="contains")
     points_id = points_id[["nodeID", "geometry"]]
     points_id.crs = "epsg:4326"
-    points_id.to_file(
-        path / Path("output") / Path("points_id.shp")
-    )  # opcional guardarlo
+    # points_id.to_file(output_path / Path("points_id.shp"))  # opcional guardarlo
     points_id_coor = gpd.GeoSeries(points_id["geometry"])
 
     ## Calcular áreas de las subcuencas intersectadas con el grid
@@ -68,8 +84,7 @@ def main():
     ## Cálculo de valores medios por año
     for i in range(2020, 2025):
         var = xr.open_dataset(
-            path
-            / Path("input")
+            input_path
             / Path(f"era5_total_precipitation_{i}_hourly_118W-86W_14N-34N_ensemble.nc")
         )
 
@@ -104,7 +119,7 @@ def main():
         else:    
             val_mean.to_zarr(path + "output/prec_mean.zarr", append_dim="time")
         """
-        val_mean.to_netcdf(path / Path("output") / Path(f"prec_mean_{i}.nc"))
+        # val_mean.to_netcdf(path / Path("output") / Path(f"prec_mean_{i}.nc"))
         break
 
     print(f"Elapsed time: {time() - t0:.3f} seconds")
--- src/xarray_hydro/watershed_utils.py
+++ src/xarray_hydro/watershed_utils.py
@@ -2,15 +2,17 @@ import numpy as np
 import geopandas as gpd
 import xarray as xr
 import polygongrid as pg
+import pyproj
 
 
 def _calculate_res(coords_arr_np: np.ndarray) -> float | None:
     if coords_arr_np is not None and len(coords_arr_np) >= 2:
-        # Ensure consistent dtype for subtraction, then convert to float
-        res = np.abs(
-            coords_arr_np.max().astype(float) - coords_arr_np.min().astype(float)
-        ) / (len(coords_arr_np) - 1)
+        coords_min = coords_arr_np.min().astype(float)
+        coords_max = coords_arr_np.max().astype(float)
+        res = (coords_max - coords_min) / (len(coords_arr_np) - 1)
         return float(res)
+    else:
+        return None
 
 
 def get_grid_from_dataset(
@@ -18,8 +20,9 @@ def get_grid_from_dataset(
     x_coords: str = "longitude",
     y_coords: str = "latitude",
 ) -> gpd.GeoDataFrame:
-    """take an xarray data structure as an input and return a vector grid as a geodataframe."""
+    """Take an xarray data structure as an input and return a vector grid as a geodataframe."""
 
+    # Get all grid input values from the xarray dataset
     res_x = _calculate_res(dataset[x_coords].values)
     res_y = _calculate_res(dataset[y_coords].values)
 
@@ -31,11 +34,102 @@ def get_grid_from_dataset(
     bounds = (x_min, x_max, y_min, y_max)
     dim_size = (len(dataset[x_coords]), len(dataset[y_coords]))
 
+    # Create the geojson. Any inconsistencies between inputs should raise an error
+    # Might be possible to implement this in house. polygongrid package seems unmaintained.
     my_grid = pg.PolygonGrid(
         bounds, step_size=(res_x, res_y), dim_size=dim_size, properties="grid"
     )
     my_grid.build_grid()
     my_grid.build_geojson()
 
-    grid = gpd.GeoDataFrame.from_features(my_grid.geojson["features"])
-    return grid
+    # GeoJSON to geopandas
+    ds_crs = pyproj.CRS.from_wkt(dataset.attrs["crs_wkt"])
+    df_grid = gpd.GeoDataFrame.from_features(my_grid.geojson["features"], crs=ds_crs)
+    return df_grid
+
+
+def get_intersected_areas(
+    intersect: gpd.GeoDataFrame, catchment_id: str
+) -> gpd.GeoDataFrame:
+    """Return areas of the intersection between the catchments and the raster grid.
+    If input CRS is not projected, a custom Equal Earth projection is used with:
+    - same ellipsoid as the input CRS,
+    - latitude of origin centered on the region of interest
+    """
+    if intersect.crs.is_geographic:
+        # Get data from input
+        ellipsoid = intersect.crs.ellipsoid
+        min_lon, _, max_lon, _ = intersect.total_bounds
+        mean_lon = (min_lon + max_lon) / 2
+        # Create custom CRS
+        prime_meridian = pyproj.crs.datum.CustomPrimeMeridian(longitude=mean_lon)
+        eq_conversion = pyproj.crs.CoordinateOperation.from_string("+proj=eqearth")
+        custom_datum = pyproj.crs.datum.CustomDatum(
+            ellipsoid=ellipsoid, prime_meridian=prime_meridian
+        )
+        crs_eqearth = pyproj.crs.ProjectedCRS(
+            name=f"Equal Earth projection on {ellipsoid.name} ellipsoid "
+            "and custom prime meridian",
+            conversion=eq_conversion,
+            geodetic_crs=pyproj.crs.GeographicCRS(datum=custom_datum),
+        )
+        assert crs_eqearth.is_projected
+        # Apply projection
+        intersect = intersect.to_crs(crs_eqearth)
+
+    # Compute the area
+    intersect["intersected_area"] = intersect.area
+    # Keep only catchment ID and calculated area
+    intersect_area = intersect[[catchment_id, "intersected_area"]]
+    return intersect_area
+
+
+def get_intersect_nodes(intersect: gpd.GeoDataFrame):
+    """⚠️ TODO"""
+    ## Generar puntos representativos en los poligonos resultantes de la intersección entre cuencas y el grid.
+    intersection_representative_points = intersect.representative_point()
+    d = {
+        "No": np.arange(0, len(intersect)),
+        "geometry": intersection_representative_points,
+    }  # ❓ why doing that?
+    intersection_representative_points = gpd.GeoDataFrame(d, crs=intersect.crs)
+    # print()
+    assert intersection_representative_points.crs is not None
+
+    ## Copiar el nodeID de las áreas intersectadas a los puntos representativos
+    points_id = intersect.sjoin(
+        intersection_representative_points, how="right", predicate="contains"
+    )
+    points_id = points_id[["nodeID", "geometry"]]
+    assert points_id.crs is not None
+    points_id_coor = gpd.GeoSeries(points_id["geometry"])
+    assert points_id_coor.crs is not None
+
+
+def get_mean_values(
+    dataset: xr.Dataset | xr.DataArray,
+    catchments: gpd.GeoDataFrame,
+    catchment_id: str,
+    x_coords: str = "longitude",
+    y_coords: str = "latitude",
+) -> xr.Dataset:
+    """Return the mean value of each dataset variable and each catchment.
+    CRS of the dataset is taken from the attribute 'crs_wkt'.
+    """
+    try:
+        dataset_crs = pyproj.CRS.from_wkt(dataset.attrs["crs_wkt"])
+    except Exception:
+        raise ValueError("'dataset' must have a parsable 'crs_wkt' attribute.")
+    if catchments.crs is None:
+        raise ValueError("'catchments' must have a crs.")
+    if catchments.crs != dataset_crs:
+        raise ValueError("'catchments' and 'dataset' crs must match.")
+
+    grid = get_grid_from_dataset(dataset, x_coords, y_coords)
+    catchments_grid_intersections = grid.overlay(catchments, how="intersection")
+
+    intersected_areas = get_intersected_areas(
+        catchments_grid_intersections, catchment_id
+    )
+    print(intersected_areas)
+    return None
